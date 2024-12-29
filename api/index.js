66f723a8a23feead46b2d462d9f62e046797a5f7
MubeenAnjum <mubeenanjum264@gmail.com>
2024-12-29 11:57:49 +0500

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
+
 const app = express();
 
 app.listen(3000, () => {
