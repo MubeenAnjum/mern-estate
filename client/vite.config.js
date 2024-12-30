097e11ab623fabb72ae585248233a628a60ce9cb
MubeenAnjum <mubeenanjum264@gmail.com>
2024-12-30 18:03:29 +0500

    Added Redux Toolkit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000', // Backend server
+        changeOrigin: true,
         secure: false,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Optional alias for convenience
     },
   },
   plugins: [react()],
